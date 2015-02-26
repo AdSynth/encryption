7b3f34565fa6130b1c9aed4346fd3f95139d7d21
Adam Thorsen <awt@fastmail.fm>
2015-02-26 11:34:03 -0800

    Fixed encryption routine to allow encryption of longer strings

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var iv = null;
 
 var encrypt = function(inputText) {
 	var cipher = crypto.createCipheriv(cipherId, key, iv);
-	cipher.update(inputText, 'utf8', 'base64');
-	var encryptedString = cipher.final('base64');
+	var encryptedString = cipher.update(inputText, 'utf8', 'base64');
+	encryptedString += cipher.final('base64');
 	var urlSafeString = encryptedString.replace(/[+]/g, '-').replace(/[/]/g, '_').replace(/[=]/g, '');
 	return urlSafeString;
 };
 
 var decrypt = function(encryptedText) {
 	var decipher = crypto.createDecipheriv(cipherId, key, iv);
-	decipher.update(encryptedText, 'base64', 'utf8');
-	var txt = decipher.final('utf8');
+	var txt = decipher.update(encryptedText, 'base64', 'utf8');
+	txt += decipher.final('utf8');
 	return txt;
 };
 
